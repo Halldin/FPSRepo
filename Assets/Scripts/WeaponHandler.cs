1ed601b3051a23651d90365eb8ab9272abf66649
Elkotte404 <elkotteonyt@gmail.com>
2024-04-04 21:25:49 +0200

    dawdsawdasaw

--- Assets/Scripts/WeaponHandler.cs
+++ Assets/Scripts/WeaponHandler.cs
@@ -18,27 +18,61 @@ public enum WeaponState
 public class WeaponHandler : MonoBehaviour
 {
     [SerializeField] PlayerMovement myPlayerMovement = null;
-    [SerializeField] Weapon[] availableWeapons;
     [SerializeField] Weapon currentWeapon = null;
+    [SerializeField] Weapon[] availableWeapons;
+    int[] ammo = new int[(int)WeaponState.Total];
 
     [SerializeField] float scrollWheelBreakpoint = 1.0f;
     private float scrollWheelDelta = 0.0f;
     int currentWeaponIndex;
+    float lastShot;
 
     
     public void Start()
     {  
-    
+        for(int i = 0; i < ammo.Length; i++){
+            ammo[i] = availableWeapons[currentWeaponIndex].ammunition;
+        }
     }
     public void Update()
     {
         HandleWeaponSwap();
         currentWeapon = availableWeapons[currentWeaponIndex];
 
-        if (Input.GetMouseButtonUp(0) && currentWeapon != null)
-        {
-            currentWeapon.Shoot();
+        if(currentWeapon.fireRate < Time.time && ammo[currentWeaponIndex] > 0){
+            StartCoroutine(FireRoutine());
         }
+    }
+
+    IEnumerator FireRoutine(){
+        switch(currentWeapon.fireMode){
+            case FireMode.Single:
+                if(Input.GetButtonDown("Fire1")){
+                    FireWeapon();
+                }
+                break;
+
+            case FireMode.Auto:
+                if(Input.GetButton("Fire1")){
+                    FireWeapon();
+                }
+                break;
+            case FireMode.Burst:
+                if(Input.GetButtonDown("Fire1")){
+                    for(int i = 0; i < currentWeapon.burstAmmount; i++){
+                        FireWeapon();
+                        yield return new WaitForSeconds(currentWeapon.burstIntervall);
+                    }
+                }
+                break;
+        }
+        yield return null;
+    }
+
+    void FireWeapon(){
+        lastShot = Time.time;
+        currentWeapon.Shoot();
+        ammo[currentWeaponIndex]--;
     }
 
     private void HandleWeaponSwap()
--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class Weapon : ScriptableObject
 {
+    public WeaponState weaponType;
     public float damagePerPellet;
     public int pelletsPerShot;
     public int ammunition;
     public float fireRate;
     public float reloadTime;
     public float accuracy;
+    public FireMode fireMode;
+    public float burstAmmount;
+    public float burstIntervall;
 
-    public void OnEnable(){
-        Reload();
-    }
-
-    public void Reload(){
-        
-    }
 
     public virtual void Shoot(){}
+}
+
+public enum FireMode{
+    Single, 
+    Auto,
+    Burst
 }
